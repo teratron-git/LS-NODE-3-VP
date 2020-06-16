3739ef4ad9ec032b5ab97b6891d8c8244847a017
Tereshkov Sergei[D] <rat87@mail.ru>
2020-06-16 18:08:57 +0300

    feat: add all API routes (dummy)

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,60 @@
 const express = require('express');
 const userCtrl = require('../controllers/userCtrl');
 const authMdwr = require('../middleware/auth');
-
 const router = express.Router();
 
 router.post('/registration', userCtrl.registration);
-
 router.post('/login', userCtrl.logIn);
+
+//TODO
+router.post('/refresh-token', (req, res) => {
+  console.log('POST /api/refresh-token');
+  res.send('POST /api/refresh-token');
+});
+
+router.get('/profile', (req, res) => {
+  console.log('GET /api/profile');
+  res.send('GET /api/profile');
+});
+
+router.patch('/profile', (req, res) => {
+  console.log('PATCH /api/profile');
+  res.send('PATCH /api/profile');
+});
+
+router.delete('/users/:id', (req, res) => {
+  console.log('DELETE /api/users/:id');
+  res.send('DELETE /api/users/:id');
+});
+
+router.get('/news', (req, res) => {
+  console.log('GET /api/news');
+  res.send('GET /api/news');
+});
+
+router.post('/news', (req, res) => {
+  console.log('POST /api/news');
+  res.send('POST /api/news');
+});
+
+router.patch('/news/:id', (req, res) => {
+  console.log('PATCH /api/news/:id');
+  res.send('PATCH /api/news/:id');
+});
+
+router.delete('/news/:id', (req, res) => {
+  console.log('DELETE /api/news/:id');
+  res.send('DELETE /api/news/:id');
+});
+
+router.get('/users', (req, res) => {
+  console.log('GET /api/users');
+  res.send('GET /api/users');
+});
+
+router.patch('/users/:id/permission', (req, res) => {
+  console.log('PATCH /api/users/:id/permission');
+  res.send('PATCH /api/users/:id/permission');
+});
 
 module.exports = router;
