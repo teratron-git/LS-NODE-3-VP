0f0acfb5b35c0d0e8086bcdc1582333a7b72abee
Tereshkov Sergei[D] <rat87@mail.ru>
2020-06-20 20:30:00 +0300

    fix: move dbConnect to top

--- server/app.js
+++ server/app.js
@@ -7,6 +7,19 @@ const routes = require('./routes');
 const app = express();
 const config = require('../config/serverConfig');
 
+mongoose
+  .connect(config.dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    const server = app.listen(process.env.PORT || config.port, function () {
+      console.log(`Сервер запущен на порту ${server.address().port}...`);
+    });
+  });
+
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
@@ -24,16 +37,3 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.render('error', { message: err.message });
 });
-
-mongoose
-  .connect(config.dbUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then(() => {
-    const server = app.listen(process.env.PORT || config.port, function () {
-      console.log(`Сервер запущен на порту ${server.address().port}...`);
-    });
-  });
