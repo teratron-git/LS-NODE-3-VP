95a04637e82a36c9f82fb779323c8b2f6076b42c
Tereshkov Sergei[D] <rat87@mail.ru>
2020-06-26 23:28:06 +0300

    fix: add missing next() in resize middleware

--- server/middleware/resize.js
+++ server/middleware/resize.js
@@ -3,27 +3,26 @@ const jimp = require('jimp');
 const path = require('path');
 
 module.exports = async (req, _, next) => {
-  const avatar = req.file ? req.file.path : null;
+  try {
+    const avatar = req.file ? req.file.path : null;
+    if (avatar) {
+      const newImage = path.join(
+        __dirname,
+        '..',
+        '..',
+        'build',
+        'uploads',
+        req.file.filename
+      );
 
-  if (avatar) {
-    const newImage = path.join(
-      __dirname,
-      '..',
-      '..',
-      'build',
-      'uploads',
-      req.file.filename
-    );
-
-    try {
       jimp.read(newImage, (err, lenna) => {
         if (err) throw err;
         lenna.resize(240, 240).write(newImage);
       });
-      next();
-    } catch (err) {
-      console.log(err);
-      next(err);
     }
+  } catch (err) {
+    console.log(err);
+    next(err);
   }
+  next();
 };
