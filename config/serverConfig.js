e3eb999b53f640656eb65bf47d543eef24a018fa
Tereshkov Sergei[D] <rat87@mail.ru>
2020-06-30 19:33:14 +0300

    perf: change serverConfig settings

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -1,19 +1,17 @@
 module.exports = {
   port: 3000,
-  dbUrlProd:
-    'mongodb+srv://admin:admin@ls-node-3-vp-uu18v.gcp.mongodb.net/LS-NODE-3-VP',
-  dbUrlDev:
-    'mongodb+srv://admin:admin@ls-node-3-vp-uu18v.gcp.mongodb.net/LS-NODE-3-VP',
+  dbUrlProd: 'mongodb+srv://admin:admin@ls-node-3-vp-uu18v.gcp.mongodb.net/LS-NODE-3-VP',
+  dbUrlDev: 'mongodb+srv://admin:admin@ls-node-3-vp-uu18v.gcp.mongodb.net/LS-NODE-3-VP',
   jwt: {
     secret: 'test',
     tokens: {
       access: {
         type: 'access',
-        expiresIn: '5m',
+        expiresIn: '60m',
       },
       refresh: {
         type: 'refresh',
-        expiresIn: '60m',
+        expiresIn: '10080m',
       },
     },
   },
